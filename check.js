fe6ec918591bc6119f1a1abe5a22bf9379336636
nehakantheti <nehakantheti@gmail.com>
2024-05-14 12:17:31 +0530

    First Commit - Adding basic files

---
+++ check.js
@@ -0,0 +1,2 @@
+const express = require('express');
+console.log('Express version:', require('express/package.json').version);
---
+++ login_page/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine','ejs');
+
+//load static assets
+app.use('/static',express.static(path.join(__dirname,'public')))
+app.use('/static',express.static(path.join(__dirname,'public/assets')))
+
+//home route
+app.get('/',(req,res)=>{
+    res.render('base',{title: "Login System"});
+})
+
+app.listen(port,()=>{console.log("Listening to the server on http://localhost:3000")})
